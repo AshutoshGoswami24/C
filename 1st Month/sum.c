23edd3997bde8a64065dd74d02be484a7cf97042
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-09-22 22:03:21 +0530

    Create sum.c

---
+++ 1st Month/sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("enter num1 num2 :");
+    scanf("%d %d",&a,&b);
+    c = a + b;
+    printf("this is sum of %d + %d = %d",a,b,c);
+    return 0;
+}
